ea7ffb5a599c41435ea5c8b774b3cfaaacfa5c7e
Maveric4 <maveriok@student.agh.edu.pl>
2019-12-22 15:18:50 +0100

    initial commit

---
+++ runSim.sh
@@ -0,0 +1,3 @@
+
+#START "" "C:\Users\DanielGacek\Documents\Repos\Carla\CarlaUE4.exe" -quality-level=Low -windowed
+START "" "C:\Users\DanielGacek\Documents\Repos\Carla\CarlaUE4.exe" ./Game/Maps/Town03 -quality-level=Low -windowed
